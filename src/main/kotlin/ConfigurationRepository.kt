11901187557c3fed7ddfaed38fd84a25de6206e4
Conclure <63319713+Conclure@users.noreply.github.com>
2023-09-20 19:58:11 +0200

    Make ConfigurationRepository.kt iterable and implement parsing functionality for registerConfiguration()

--- src/main/kotlin/ConfigurationRepository.kt
+++ src/main/kotlin/ConfigurationRepository.kt
@@ -1,4 +1,4 @@
-class ConfigurationRepository(private val map: MutableMap<String, Configuration>) {
+class ConfigurationRepository(private val map: MutableMap<String, Configuration>) : Iterable<Configuration>{
 
     companion object {
         @JvmStatic
@@ -7,13 +7,27 @@ class ConfigurationRepository(private val map: MutableMap<String, Configuration>
         }
     }
 
-    fun registerConfiguration(configuration: Configuration) {
+    fun registerConfiguration(configuration: Configuration): Boolean {
+        try {
+            configuration.studySessionDuration.maxAsDuration()
+            configuration.studySessionDuration.minAsDuration()
+            configuration.breakSessionDuration.maxAsDuration()
+            configuration.breakSessionDuration.minAsDuration()
+        } catch (e: IllegalArgumentException) {
+            return false;
+        }
         if (this.map[configuration.name] == null) {
             this.map[configuration.name] = configuration
+            return true
         }
+        return false
     }
 
-    fun deregisterConfiguration(configurationName: String) {
-        this.map.remove(configurationName)
+    fun deregisterConfiguration(configurationName: String): Boolean{
+        return this.map.remove(configurationName) == null;
+    }
+
+    override fun iterator(): Iterator<Configuration> {
+        return this.map.values.iterator()
     }
 }
